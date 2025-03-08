2ee6bcacc082009955b4544c093ffa707956d4c0
Md Imran Hasan <mhasa02@jaguar.tamu.edu>
2025-03-08 13:32:05 -0600

    script.js

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
-  document.querySelectorAll(".navbar ul li a").forEach(link => {
+  document.querySelectorAll("nav ul li a").forEach(link => {
     link.addEventListener("click", function (e) {
       e.preventDefault();
       const targetId = this.getAttribute("href").substring(1);
       document.getElementById(targetId).scrollIntoView({ behavior: "smooth" });
     });
-  });
-
-  document.getElementById("ResumeBtn").addEventListener("click", function () {
-    window.open("cv.pdf", "_blank");
   });
 });
